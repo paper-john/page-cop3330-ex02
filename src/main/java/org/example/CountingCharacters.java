d437e179232c33574c95e1476a58c536a4e09bc2
John Page <johncalvinpage@gmail.com>
2021-09-13 12:17:49 -0400

    Exercise 2

--- src/main/java/org/example/CountingCharacters.java
+++ src/main/java/org/example/CountingCharacters.java
@@ -3,11 +3,13 @@
  *  Copyright 2021 John Page
  */
 
+//test
+
 package org.example;
 
 import java.util.Scanner;
 
-public class App
+public class CountingCharacters
 {
     public static void main( String[] args )
     {
